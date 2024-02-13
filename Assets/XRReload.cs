9a93e8800249ce28e10bf8701450b16fb258c541
FLUEGNAT <FLUEGNAT@psd267.org>
2024-02-13 10:29:37 -0800

    penis sex balls

--- Assets/XRReload.cs
+++ Assets/XRReload.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Management;
 
 public class XRReload : MonoBehaviour
 {
-    public void Awake()
+    public async void Awake()
     {
+        await Task.Delay(10);
         if (XRGeneralSettings.Instance.Manager.activeLoader != null)
         {
             XRGeneralSettings.Instance.Manager.StopSubsystems();
